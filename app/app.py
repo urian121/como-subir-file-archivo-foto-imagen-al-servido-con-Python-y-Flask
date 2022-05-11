d7d42e535e77736ba5642fad7c61262fe15749c2
URIAN VIERA <urian1213viera@gmail.com>
2022-05-10 22:09:05 -0500

    Add files via upload
    ajustando

--- app/app.py
+++ app/app.py
@@ -20,7 +20,7 @@ def stringAleatorio():
     #Generando string aleatorio
     string_aleatorio = "0123456789abcdefghijklmnopqrstuvwxyz_"
     longitud         = 20
-    secuencia       = string_aleatorio.upper()
+    secuencia        = string_aleatorio.upper()
     resultado_aleatorio  = sample(secuencia, longitud)
     string_aleatorio     = "".join(resultado_aleatorio)
     return string_aleatorio
